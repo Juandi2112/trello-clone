b2e47a4d68acc479cc6c67008b25277ecc11e7d2
Juan <juandi.21.jdj@gmail.com>
2024-02-14 17:50:57 -0500

    Fixed missing name on CancelButton

--- src/components/CancelButton.tsx
+++ src/components/CancelButton.tsx
@@ -2,7 +2,7 @@ import { faClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-export default function({onClick}:{onClick:()=>void}){
+export default function CancelButton({onClick}:{onClick:()=>void}){
     return(
         <button
         className="mt-4 gap-2 items-center w-full flex justify-center uppercase text-sm text-gray-400"
