9fae4ea6e40bc2d3e6c1a93c1fdb269fe5b31ffa
Juan <juandi.21.jdj@gmail.com>
2024-03-05 16:31:02 -0500

    Changes on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default async function Header() {
     <header className="bg-gray-200 p-4 px-8">
       <div className="flex justify-between items-center">
         <Link href="/" className="logo">
-          Trello
+          Tasks Manager
         </Link>
         <div>
           {session && (
